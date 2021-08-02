a426c89a0bde2ddd5d70cca111a04dd7d6789b3c
Dreezy305 <bankoleidris@gmail.com>
2021-08-02 22:49:04 +0000

    another commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "mobx-react";
+
+const Root = (
+  <Provider>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/stores/birdStore.js
+++ src/stores/birdStore.js
@@ -8,7 +8,7 @@ class BirdStore {
   };
 
   @computed get birdCount = () => {
-      return this.birs.size;
+      return this.birds.size;
   }
 }
 

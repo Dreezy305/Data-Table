bf184710ecc735446bf16cbdf2d0a9110f5aca24
Dreezy305 <bankoleidris@gmail.com>
2021-08-02 15:06:36 +0000

    another commit

--- src/store.js
+++ src/store.js
@@ -40,3 +40,9 @@ class ObservableTodoStore {
 }
 
 const observableTodoStore = new ObservableTodoStore();
+
+observableTodoStore.addTodo("read MobX tutorial");
+observableTodoStore.addTodo("try MobX");
+observableTodoStore.todos[0].completed = true;
+observableTodoStore.todos[1].task = "try MobX in own project";
+observableTodoStore.todos[0].task = "grok MobX tutorial";
